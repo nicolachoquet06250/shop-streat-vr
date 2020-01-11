dc2c59cb5418be7add22f2daa5f97253699f4d45
Nicolas Choquet <nicolachoquet06250@gmail.com>
2020-01-11 15:54:03 +0100

    teleportation on a case and inside a building is ok

--- aframe/index.php
+++ aframe/index.php
@@ -1,4 +1,9 @@
 <?php
 
+header('Content-Type: application/json');
 $contents = file_exists(__DIR__.'/test.log') ? file_get_contents(__DIR__.'/test.log') : '';
 file_put_contents(__DIR__.'/test.log', $contents."\n".$_GET['message']);
+
+echo json_encode([
+    'success' => true,
+]);
